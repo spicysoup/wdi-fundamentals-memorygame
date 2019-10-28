8d4f150281c393892a8c79ca9f58fa6e79be77f3
Zhuang Xu <zxu@localhost.localdomain>
2019-10-28 11:10:31 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,23 @@
-console.log('Up and running!');
-
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log(`User flipped ${cardOne}`);
-console.log(`User flipped ${cardTwo}`);
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert('You found a match');
-    } else {
-        alert('Sorry, try again.');
+function checkForMatch() {
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            console.log('You found a match');
+        } else {
+            console.log('Sorry, try again.');
+        }
     }
 }
+
+function flipCard(id) {
+    console.log(`User flipped ${cards[id]}`);
+
+    cardsInPlay.push(cards[id]);
+
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
