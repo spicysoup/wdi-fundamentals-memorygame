acdd1bb0350246e5d99e7f02540e2d3b5582af3e
Zhuang Xu <zxu@localhost.localdomain>
2019-10-28 18:54:34 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -29,10 +29,19 @@ function checkForMatch() {
         } else {
             alert('Sorry, try again.');
         }
+        reset();
     }
 }
 
-function flipCard(cardId) {
+function flipCard(event) {
+    const card = event.target;
+    if (card.tagName.toLowerCase() !== 'img') {
+        return;
+    }
+
+    const cardId = parseInt(card.getAttribute('data-id'));
+    card.setAttribute('src', cards[cardId].cardImage);
+
     console.log(`User flipped ${cards[cardId].rank} of ${cards[cardId].suit} at ${cards[cardId].cardImage}`);
 
     cardsInPlay.push(cards[cardId].rank);
@@ -40,5 +49,28 @@ function flipCard(cardId) {
     checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+    const gameBoard = document.getElementById('game-board');
+
+    cards.forEach((card, i) => {
+        const cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i.toString());
+
+        gameBoard.appendChild(cardElement);
+    });
+
+    gameBoard.addEventListener('click', flipCard);
+
+    document.querySelector('.reset').addEventListener('click', reset);
+}
+
+function reset() {
+    cardsInPlay = [];
+    const cardElements = document.querySelectorAll('img');
+    cardElements.forEach(cardElement => {
+        cardElement.setAttribute('src', 'images/back.png');
+    });
+}
+
+createBoard();
